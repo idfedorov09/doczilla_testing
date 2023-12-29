8d6fb27adc5c05d717a2ff9bfd724572aed58600
Ivan <idfedorov09@gmail.com>
2023-12-29 19:35:18 +0300

    fix gitignore

--- problem_1/src/main/java/ru/doczilla/testing/Main.java
+++ problem_1/src/main/java/ru/doczilla/testing/Main.java
@@ -3,8 +3,27 @@ package ru.doczilla.testing;
 public class Main {
 
     public static void main(String[] args) {
-        new FileDesc("./test_folder2")
+        String workdir = getPath(args);
+
+        new FileDesc(workdir)
                 .concatenateAllFiles();
+    }
+
+    private static String getPath(String[] args) {
+        String folderPath = null;
+
+        for (String arg : args) {
+            if (arg.startsWith("--folder=")) {
+                folderPath = arg.substring("--folder=".length());
+                break;
+            }
+        }
+
+        if (folderPath == null) {
+            folderPath = ".";
+        }
+
+        return folderPath;
     }
 
 }
