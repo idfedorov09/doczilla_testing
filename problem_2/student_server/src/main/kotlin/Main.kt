0575559955c11e7c130514746266a4271a077191
Ivan <idfedorov09@gmail.com>
2023-12-31 01:07:57 +0300

    cors - allowed to any host

--- problem_2/student_server/src/main/kotlin/Main.kt
+++ problem_2/student_server/src/main/kotlin/Main.kt
@@ -1,13 +1,27 @@
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
     configureRouting()
+    configureCors()
     configureSerialization()
+}
+
+fun Application.configureCors() {
+    install(CORS) {
+        anyHost()
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Delete)
+        allowHeader(HttpHeaders.ContentType)
+        allowHeaders { true }
+    }
 }
 
 fun Application.configureRouting() {
